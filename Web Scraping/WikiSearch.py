bf02d3c5675859aa89dd16b47b9bca672e19d019
Daniel Moura <92269045+DanielMoura1991@users.noreply.github.com>
2021-10-10 15:38:24 -0300

    Create WikiSearch.py

---
+++ Web Scraping/WikiSearch.py
@@ -0,0 +1,43 @@
+import PySimpleGUIQt as sg
+from bs4 import BeautifulSoup as bs
+import requests as rq
+
+layout = [[sg.Text(" Digite o tema a ser pesquisado:")],
+          [sg.Input(key='Input')],
+          [sg.Output(key='Output')],
+          [sg.Button('Pesquisar', key='Search'), sg.Button('Limpar', key='Clear'), sg.Button('Sair', key='Quit')]]
+
+window = sg.Window('WikiSearch v1.0 - by Daniel Moura', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    elif event == 'Clear':
+        window['Input'].update('')
+        window['Output'].update('')
+    else:
+        busca = values['Input']
+        busca = busca.replace(" ", '_')
+        url = "https://pt.wikipedia.org/wiki/"+busca
+        try:
+            page = rq.get(url=url)
+            soup = bs(page.content, 'html.parser')
+            cont = soup.find(id='mw-content-text')
+            texto = cont.get_text()
+            window['Output'].update(f'{texto}')
+
+            with open(busca+'.txt', 'wb') as f:
+
+                for cont in soup.find_all('p'):
+                    texto = cont.get_text()
+                    print(texto)
+                    f.write(texto.encode())
+            f.close()
+
+        except:
+            
+            window['Output'].update('Erro de Conexão! Tente outra hora')
+
+window.close()
